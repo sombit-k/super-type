b042c463cd79b39186a85280fd2ed781c8dd4266
sombit-k <sombitkarmakar018@gmail.com>
2025-06-27 19:04:45 +0530

    Enhance Instructions component with additional guidance; update layout for better clarity and structure.

--- components/Instructions.tsx
+++ components/Instructions.tsx
@@ -1,5 +1,6 @@
+"use server"
 import React from 'react'
-
+import Link from 'next/link'
 const Instructions = ({ id }: { id: string }) => {
     return (
         <>
@@ -19,11 +20,35 @@ const Instructions = ({ id }: { id: string }) => {
 
                 <div className="flex flex-col gap-4 h-full border-2 border-white-500 text-amber-50 m-3.5  rounded-xl shadow-xl-20 shadow-[0_0_6px_1px_white]">
 
-                    <div className="bg-pink-600  h-1/10 mx-3 mt-3">
-                        Instructions
+                    <div className="bg-pink-600  h-1/10 mx-3 mt-3 flex justify-center items-center">
+                        <div>
+                            Instructions
+                        </div>
                     </div>
-                    <div className="bg-pink-800 h-9/10 mx-3 mb-3">02</div>
+                    <div className="flex flex-row bg-pink-800 h-7/10 mx-3 ">
+                        <div className="w-1/2 bg-pink-600 p-3 border mr-0.5">
+                        <h1>Some general Instructions to keep in mind:</h1>
+                        <ul className="list-disc pl-5">
+                            <li>Make sure to place the fingers on the correct orientation</li>
+                            <li>Click on the Start button below to start this excercise</li>
+                            <li>Timer will start automatically once the first letter is placed</li>
+                            <li>Try finishing this exercise within 60 seconds with no mistakes before moving to the next exercise</li>
+                            
+                        </ul>
+
+                        </div>
+                        <div className="w-1/2 bg-pink-600 p-3 border ml-0.5">2</div>
+                    </div>
+                    <div className="bg-pink-600  h-1/10 mx-3 mb-3 flex justify-center items-center">
+                        <div>
+                            <Link href={`/practice/${id}`} className="lsnbtn" key={id} >
+                                Start This excercise
+                            </Link>
+                        </div>
+                    </div>
+
                 </div>
+
 
             </div>
         </>
