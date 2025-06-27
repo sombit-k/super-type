df84072226933e0989c977d5df4a13a9c76e791e
sombit-k <sombitkarmakar018@gmail.com>
2025-06-27 18:31:17 +0530

    Refactor Instructions component layout; update styling for better structure and clarity.

--- components/Instructions.tsx
+++ components/Instructions.tsx
@@ -19,12 +19,10 @@ const Instructions = ({ id }: { id: string }) => {
 
                 <div className="flex flex-col gap-4 h-full border-2 border-white-500 text-amber-50 m-3.5  rounded-xl shadow-xl-20 shadow-[0_0_6px_1px_white]">
 
-                    <div className="bg-pink-600  h-1/2">
-                        01
+                    <div className="bg-pink-600  h-1/10 mx-3 mt-3">
+                        Instructions
                     </div>
-                        <div className="bg-pink-800">02</div>
-
-                        <div className="bg-pink-900">03</div>
+                    <div className="bg-pink-800 h-9/10 mx-3 mb-3">02</div>
                 </div>
 
             </div>
