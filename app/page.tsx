51a50b36a0bd0d1d7a2ec8c3ee36292841dcd22b
sombit-k <sombitkarmakar018@gmail.com>
2025-06-30 13:37:11 +0530

    Refactor Home, Sidebar, and Tutorial components; update styles for improved consistency and clarity, and simplify Tutorial logic.

--- app/page.tsx
+++ app/page.tsx
@@ -4,25 +4,28 @@ import Sidebar from '@/components/sidebar';
 import TypeArea from '@/components/typearea';
 import Tutorial from '@/components/tutorial';
 import Dashboard from '@/components/dashboard';
-export default async function Home(){
+export default async function Home() {
 
   return (
-    <div className="flex align-middle bg-gray-500 min-h-screen ">
+    <div className="flex align-middle bg-dark1-500 min-h-screen ">
       <div className="grid grid-cols-5 gap-4 p-4 flex-none w-full min-h-screen">
 
-        <div className="bg-gray-200 p-4 text-gray-800 rounded-lg shadow-md flex-none col-span-1">
+        <div className="bg-dark1-500 p-4 text-gray-800 rounded-lg shadow-md flex-none col-span-1 border-1 border-amber-50">
 
-          <p> objectives</p>
+          <div className="my-0 mr-3 flex flex-col items-center justify-center  font-bold text-white">
+            Start Practice!
+          </div>
           <Sidebar />
 
         </div>
 
 
-        <div className="bg-gray-200 p-4 bg text-gray-800 rounded-lg shadow-md col-span-3 col-start-2 gap-4">
-          <Tutorial id={0}/>
+        
+        <div className="bg-dark1-500 p-4 bg text-gray-800 rounded-lg shadow-md col-span-3 col-start-2 gap-4 border-1 border-amber-50">
+          <Tutorial />
         </div>
 
-        <Dashboard/>
+        <Dashboard />
 
       </div>
     </div>
--- components/tutorial.tsx
+++ components/tutorial.tsx
@@ -1,16 +1,13 @@
 import React from 'react'
 
-const Tutorial = ({ id }: { id?: number }) => {
+const Tutorial = () => {
   return (
     <>
-      <div className="flex flex-col h-4/5 bg-amber-950 text-amber-50">
-        {id === 0 && <p>Learn how to type {id}</p>}
+      <div className="flex flex-col h-full bg-dark2-500  text-amber-50">
+        <h1>Learn the basics of Touch Typing</h1>
       </div>
-      
-      <div className="flex flex-row h-1/5 bg-amber-950 text-amber-50 my-3.5 justify-around gap-4">
-        <button className="">press me</button>
-        <button className="">press me</button>
-      </div>
+
+    
     </>
   )
 }
