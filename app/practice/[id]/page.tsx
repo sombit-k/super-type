88384bae7682264769bfd6d596227fa7cb0ddba1
sombit-k <sombitkarmakar018@gmail.com>
2025-06-30 13:15:49 +0530

    Refactor styles in LessonPage and globals.css; update button background color and add border for improved visibility and consistency.

--- app/practice/[id]/page.tsx
+++ app/practice/[id]/page.tsx
@@ -7,13 +7,13 @@ export default async function LessonPage({ params }: { params: Promise<{ id: str
   const id = (await params).id;
 
   return (<>
-  <div className="flex align-middle min-h-screen bg-dark4-500">
-      <div className="grid grid-cols-5 gap-4 p-4 flex-none w-full min-h-screen">
+  <div className="flex align-middle min-h-screen bg-black-500">
+      <div className="grid grid-cols-5 gap-4 p-4 flex-none w-full min-h-screen ">
 
-        <div className="bg-dark1-500 p-4 text-gray-800 rounded-lg shadow-md flex-none col-span-1">
+        <div className="bg-dark1-500 p-4 text-gray-800 rounded-lg shadow-md flex-none col-span-1 border-1 border-amber-50">
 
-          <div className="mb-2 flex flex-col items-center justify-center  font-bold text-white">
-          <Link href={`/`} className="lsnbtn px-10" key={"home"} >
+          <div className="my-0 mr-3 flex flex-col items-center justify-center  font-bold text-white">
+          <Link href={`/`} className="lsnbtn px-12" key={"home"} >
                   Home
                 </Link>
           </div>
@@ -22,7 +22,7 @@ export default async function LessonPage({ params }: { params: Promise<{ id: str
         </div>
 
 
-        <div className="bg-dark1-500 p-4 bg text-gray-800 rounded-lg shadow-md col-span-3 col-start-2 gap-4">
+        <div className="bg-dark1-500 p-4 bg text-gray-800 rounded-lg shadow-md col-span-3 col-start-2 gap-4 border-1 border-amber-50">
           <TypeArea id={id} />
         </div>
 
